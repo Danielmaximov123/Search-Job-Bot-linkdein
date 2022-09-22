11093a543a15fd3d5a6926f1a2ec2f71f31471cd
Daniel Maximov <87207207+Danielmaximov123@users.noreply.github.com>
2022-09-22 16:21:07 +0300

    Update app.js

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.use(express.json())
         }, 60000 * 30 )
 
     setInterval( () => {
-            linkedin()
+            // linkedin()
         }, 60000 * 2 )
 
         console.log("ok");
