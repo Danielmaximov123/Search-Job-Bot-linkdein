3304f7c9229a9be8bd79b727997e456ec01b186e
Daniel Maximov <87207207+Danielmaximov123@users.noreply.github.com>
2022-11-14 15:49:51 +0200

    Update EmailFuncWorkBL.js

--- mails/EmailFuncWorkBL.js
+++ mails/EmailFuncWorkBL.js
@@ -4,7 +4,7 @@ exports.checkWorkEmail = () => {
     return new Promise((resolve , reject) => {
         let mailOptions = {
             from: '"A bot looking for a job" <No-Reply@splitInvoice.com>', 
-            to: 'DanielMaximov2@gmail.com', 
+            to: 'My-mail', 
             subject: `The bot still works`, 
             html: `
             <div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2; direction: ltr;">
